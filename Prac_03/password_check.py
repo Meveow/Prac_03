5813ebd479b1dd2bdfac9772b2bbde807289b2a5
Meveow <karwai.soong@jcu.edu.au>
2022-04-03 21:25:30 +0800

    Refactor temperatures program to use functions

--- Prac_03/password_check.py
+++ Prac_03/password_check.py
@@ -11,6 +11,10 @@ def get_password():
     while len(password) < MIN_LENGTH:
         print("Must meet minimum length of 5")
         password = input("Password: ")
+    print_asterisks(password)
+
+
+def print_asterisks(password):
     print(symbol * len(password))
 
 
