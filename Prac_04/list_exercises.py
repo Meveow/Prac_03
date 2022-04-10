b6f8c7ba55504763da4840d28ac3fb1dd88e95d6
Meveow <karwai.soong@jcu.edu.au>
2022-04-10 20:59:24 +0800

    Add error check in security checker program

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -1,13 +1,20 @@
-numbers = []
+# numbers = []
+#
+# times = 5
+#
+# for n in range(times):
+#     response = int(input("Number: "))
+#     numbers.append(response)
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+# print(f"The average of the numbers is {sum(numbers) / times}")
 
-times = 5
-
-for n in range(times):
-    response = int(input("Number: "))
-    numbers.append(response)
-
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
-print(f"The average of the numbers is {sum(numbers) / times}")
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
