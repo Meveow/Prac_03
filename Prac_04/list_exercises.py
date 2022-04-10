b0e0c509f009874406823c074a234f85441b9db0
Meveow <karwai.soong@jcu.edu.au>
2022-04-10 20:47:16 +0800

    Display results using functions

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -5,3 +5,9 @@ times = 5
 for n in range(times):
     response = int(input("Number: "))
     numbers.append(response)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers) / times}")
