f357b8a4229a4b2a79390e585122b3765a87b39f
Meveow <karwai.soong@jcu.edu.au>
2022-04-11 12:27:05 +0800

    Distribute functions

--- Prac_04/quick_pickys.py
+++ Prac_04/quick_pickys.py
@@ -1,7 +1,13 @@
 import random
+VALUES = []
 
 amount = int(input("How many quick picks? "))
 
 for line in range(amount):
     line = random.randint(1, 45)
-    print(line)
+    for line in range(6):
+        line = random.randint(1, 45)
+        VALUES.append(line)
+        VALUES.sort()
+        print(VALUES)
+        # print(line, end=' ')
