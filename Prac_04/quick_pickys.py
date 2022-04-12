0a7e1062255e0ff24e26712027f1493a0d16162b
Meveow <karwai.soong@my.jcu.edu.au>
2022-04-12 11:05:48 +0800

    Change list to string with join() function

--- Prac_04/quick_pickys.py
+++ Prac_04/quick_pickys.py
@@ -1,12 +1,12 @@
 import random
 
-COLUMN = 6
+LINE = 6
 
 amount = int(input("How many quick picks? "))
 
-for c in range(amount):
+for c in range(amount): #number of lines
     values = []
-    for r in range(COLUMN):
+    for r in range(LINE): #six elements in each line
         num = random.randint(1, 45)
         while num in values: #no repitition of numbers
             num = random.randint(1, 45)
