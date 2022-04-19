f8864454a71c019552b56e17ffbdbe5d4ff80cdd
Meveow <karwai.soong@jcu.edu.au>
2022-04-19 15:36:54 +0800

    Sort and align output

--- Prac_05/word_occurrences.py
+++ Prac_05/word_occurrences.py
@@ -5,10 +5,13 @@ text = input("Enter a text: ")
 print(f"Text: {text}")
 words = text.split()
 
+
 for word in words:
     try:
         words_in_text[word] += 1
     except KeyError:
         words_in_text[word] = 1
 
+for words, count in sorted(words_in_text.items()):
+    print(f"{words:10} : {count:1}")
 
