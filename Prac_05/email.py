6fb60dcde8757589e07d2bb666262d7fc8ccb58b
Meveow <karwai.soong@jcu.edu.au>
2022-04-19 18:17:06 +0800

    Finish while loop in main function

--- Prac_05/email.py
+++ Prac_05/email.py
@@ -3,7 +3,11 @@ def main():
     email_address = input("Email: ")
     while email_address != '':
         name = names(email_address)
-
+        check = input(f"Is your name {name}? (Y/N) ").lower()
+        if check == "n" or check == "no":
+            name = input("Name: ")
+        email_to_name[email_address] = name
+        email_address = input("Email: ")
 
     for email_address, name in email_to_name.items():
         print(f"{name:13}, {email_address}")
