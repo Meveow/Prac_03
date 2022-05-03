40021991cbad2448949b2cff9fcba68d44d2fa48
Meveow <karwai.soong@jcu.edu.au>
2022-05-03 19:14:11 +0800

    Edit final output

--- Prac_06/guitars.py
+++ Prac_06/guitars.py
@@ -2,28 +2,31 @@ from guitar_info import GuitarInfo
 
 
 def main():
-
     guitars = []
+
     print("My guitars!")
-    name = ''
-    while name != " ":
-        name = input("Name: ")
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-        print(f"{name} ({year}) : ${cost} added.")
-        guitars.append([name, year, cost])
-    else:
-        print("... snip ...")
+    # name = input("Name: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: $"))
+    #     add_guitar = GuitarInfo(name, year, cost)
+    #     guitars.append(add_guitar)
+    #     print(f"{name} ({year}) : ${cost} added.")
+    #     name = input("Name: ")
 
-    guitars.append(Guitar())
-    print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
-                                                              guitar.is_vintage))
+    guitars.append(GuitarInfo("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(GuitarInfo("Line 6 JTV-59", 2010, 1512.9))
 
-    # gibson = GuitarInfo("Gibson", 1922, 16035.40)
-    #
-    # print(gibson)
-    # print(f"Age: {gibson.get_age()}")
-    # print(f"Vintage: {gibson.is_vintage()}")
+    print("\n...snip...\n")
+
+    print("These are my guitars:")
+    count = 0
+    for i, guitar in enumerate(guitars, 1):
+        count += 1
+        vintage = ""
+        if guitar.is_vintage():
+            vintage = "(vintage)"
+        print(f"Guitar {count}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage}")
 
 
 main()
