5be9ad461b96222a7fb60256891d67b1eb4bdd7e
Meveow <karwai.soong@jcu.edu.au>
2022-05-09 22:13:52 +0800

    Add calculation, increase/decrease

--- Prac_07/convert_miles_km.py
+++ Prac_07/convert_miles_km.py
@@ -4,7 +4,7 @@ from kivy.lang import Builder
 M_TO_KM = 1.60934
 
 
-class ConvertMilesToKm(App):
+class Conversion(App):
     def build(self):
         self.title = "Conversion"
         self.root = Builder.load_file('convert_miles_km.kv')
@@ -13,7 +13,15 @@ class ConvertMilesToKm(App):
     def handle_calculate(self, value):
         result = float(value) * M_TO_KM
         self.root.ids.output_label.text = str(result)
-        return result
+
+    def increment(self, value):
+        increase = int(value) + 1
+        self.root.ids.input_number.text = str(increase)
+
+    def decrement(self, value):
+        decrease = int(value) - 1
+        self.root.ids.input_number.text = str(decrease)
 
 
-ConvertMilesToKm().run()
+
+Conversion().run()
