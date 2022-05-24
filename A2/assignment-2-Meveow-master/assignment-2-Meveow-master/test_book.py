1f188fe6ae4c896911431dcbc8941fa3a1241a70
Meveow <karwai.soong@jcu.edu.au>
2022-05-24 21:03:34 +0800

    Test mark_required()

---
+++ A2/assignment-2-Meveow-master/assignment-2-Meveow-master/test_book.py
@@ -0,0 +1,39 @@
+"""(Incomplete) Tests for Book class."""
+from book import Book
+
+
+def run_tests():
+    """Test Book class."""
+
+    # Test empty book (defaults)
+    # print("Test empty book:")
+    # default_book = Book()
+    # print(default_book)
+    # assert default_book.title == ""
+    # assert default_book.author == ""
+    # assert default_book.number_of_pages == 0
+    # assert not default_book.is_completed
+
+    # Test initial-value book
+    print("Test initial-value book:")
+    new_book = Book("Fish Fingers", "Dory", 501, True)
+    # TODO: Write tests to show this initialisation works
+    assert new_book.title == "Fish Fingers"
+    assert new_book.author == "Dory"
+    assert new_book.number_of_pages == 501
+    assert new_book.is_completed
+    print(new_book)
+
+    # Test mark_required()
+    new_book.mark_required()
+    # TODO: Write tests to show the mark_required() method works
+    assert not new_book.is_completed
+    print(new_book)
+
+    # Test is_long()
+    # TODO: Write tests to show the is_long() method works
+    assert new_book.number_of_pages == 501
+    # TODO: Add more tests, as appropriate
+
+
+run_tests()
